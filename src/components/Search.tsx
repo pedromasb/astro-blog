b5a74e62d89e1eef1ad2307a8a30f51b8b649db1
Pedro Mas <pedro.masb@gmail.com>
2024-09-30 15:25:18 -0700

    u

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -84,7 +84,7 @@ export default function SearchBar({ searchList }: Props) {
         </span>
         <input
           className="block w-full rounded border border-skin-fill/40 bg-skin-fill py-3 pl-10 pr-3 placeholder:italic focus:border-skin-accent focus:outline-none"
-          placeholder="Search for anything..."
+          placeholder="Search all posts..."
           type="text"
           name="search"
           value={inputVal}
