2275081bac6092e760ac69add6ec423e500ad671
Pedro Mas <pedro.masb@gmail.com>
2024-09-30 15:53:11 -0700

    u

--- src/assets/socialIcons.ts
+++ src/assets/socialIcons.ts
@@ -67,10 +67,10 @@ const socialIcons = {
   </svg>`,
 
   /* others */
-  Personal: `<svg xmlns="http://www.w3.org/2000/svg" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user">
+  Personal: `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-user">
   	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-  	<path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-  	<path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+  	<path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
+  	<path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" />
   </svg>`,
 	
   ESA: `<svg 
