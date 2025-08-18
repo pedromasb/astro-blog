0246e5388370826a2060401eedff62e2a8411c17
Pedro Mas <pedro.masb@gmail.com>
2025-08-18 14:40:05 +0000

    chatbot fix

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
@@ -8,6 +9,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output:"server",
+  adapter: vercel(),
   site: SITE.website,
   integrations: [
     tailwind({
